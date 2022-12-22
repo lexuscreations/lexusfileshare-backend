7e7b2f7d6d335f0c8f2b93bb1cb6cc823889e24c
LokeshVishwakarma <lokeshvishwakarma2611@gmail.com>
2022-12-23 00:26:05 +0530

    update-cors-socket-config-domains-array

--- config/index.js
+++ config/index.js
@@ -1,7 +1,8 @@
 module.exports = {
   socketConfig: {
     cors: {
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+      credentials: true,
     },
   },
 };
